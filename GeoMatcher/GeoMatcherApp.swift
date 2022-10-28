b2815120506ff82a8873af27cd8260c146b74354
hazawanalytics <hazawanalytics@gmail.com>
2022-10-28 09:58:23 +0800

    Initial Commit

---
+++ GeoMatcher/GeoMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoMatcherApp.swift
+//  GeoMatcher
+//
+//  Created by Matthias on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct GeoMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
